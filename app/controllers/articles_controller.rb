e1ccf79734f042e4e42f84abc8ada0593a6664b8
arnmatu <arnas.matusevicius@gmail.com>
2017-11-12 22:03:51 -0600

    extract redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -10,12 +12,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+      #@article = Article.new(article_params)
   end
 
   def create
     #render plain: params[:article].inspect
-    @article = Article.new(article_params)
+    #@article = Article.new(article_params)
     if @article.save
       flash[:notice]="Article was successfully created"
       redirect_to article_path(@article) #from rake routes and passing article instance variable
@@ -25,7 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice]="Article was successfully updated"
       redirect_to article_path(@article)
@@ -35,11 +37,11 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
     flash[:notice]="Article was successfully deleted"
@@ -48,9 +50,12 @@ class ArticlesController < ApplicationController
 
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title,:description)
-
     end
 
 end
