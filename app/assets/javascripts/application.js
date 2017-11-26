340e1842882a898bc3e1cec19c07cbd8d11998e4
arnmatu <arnas.matusevicius@gmail.com>
2017-11-25 20:29:08 -0600

    1.0

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require jquery
 //= require bootstrap-sprockets
+//= require jquery
+//= require rails-ujs
+//= require jquery-ui
 //= require turbolinks
 //= require_tree .
+
+
