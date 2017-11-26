1985ef0f2c47a29cd953f1987f74a933abf37a5f
arnmatu <arnas.matusevicius@gmail.com>
2017-11-25 19:35:15 -0600

    0.3

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,4 @@ module AlphaBlog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 end
